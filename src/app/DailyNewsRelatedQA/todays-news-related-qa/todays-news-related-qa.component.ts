f082591c1c53b17a2ae7c30203bd21c18018caa6
JibinJF <57614642+JibinJF@users.noreply.github.com>
2020-05-06 16:12:37 +0530

    Add files via upload

---
+++ src/app/DailyNewsRelatedQA/todays-news-related-qa/todays-news-related-qa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todays-news-related-qa',
+  templateUrl: './todays-news-related-qa.component.html',
+  styleUrls: ['./todays-news-related-qa.component.css']
+})
+export class TodaysNewsRelatedQAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
