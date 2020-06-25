5d91779e98cf1b3ecbae1e4f1519f3b77cebf429
JibinJF <57614642+JibinJF@users.noreply.github.com>
2020-06-25 21:19:36 +0530

    Add files via upload

--- src/app/general-awareness-qa/history-qa/ancient-indian-history-qa/chalcolithic-farming-cultures-qa/chalcolithic-farming-cultures-qa.component.ts
+++ src/app/general-awareness-qa/history-qa/ancient-indian-history-qa/chalcolithic-farming-cultures-qa/chalcolithic-farming-cultures-qa.component.ts
@@ -18,10 +18,210 @@ export class ChalcolithicFarmingCulturesQAComponent implements OnInit {
   chalcolithicFarmingCulturesQAs: QAModel[] = [
     {
       qaNumber: this.qaNumber++,
-      question: `CQ1`,
-      answer: `CA1`,
-      description: `CD1`
-    }
+      question: `Which was the first metal to be used?`,
+      answer: `Copper`,
+      description: ``
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `The culture based on the use of copper and stone implements is called _____?`,
+      answer: `Chalcolithic`,
+      description: `<ul>
+      <li>Chalcolithic means stone-copper phase.</li>
+      <li>Though, many of the chalcolithic cultures appear after the end of the bronze based Harappan culture, 
+      chalcolithic cultures seems to be technologically less advanced than the Harappan cultures.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Occasionally, the chalcolithic people used bronze as well. State true or false?`,
+      answer: `True`,
+      description: `<ul>
+      <li>The bronze they used was found to be of low-grade.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `While the Harappans attained urbanisation, the chalcolithic people were primarily rural communities. 
+      State true or false?`,
+      answer: `True`,
+      description: `<ul>
+      <li>The chalcolithic people spread over places where hilly land and rivers were available.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `_____ is considered the richest among the chalcolithic ceramics?`,
+      answer: `Malwa ware`,
+      description: `<ul>
+      <li>Malwa ware is typical of the Malwa chalcolithic culture of central and western India.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Which chalcolithic culture covered majority of modern Maharashtra?`,
+      answer: `Jorwe culture (C. 1400 B.C to 700 B.C)`,
+      description: `<ul>
+      <li>Jorwe culture sites were located in semi-arid areas in the riverine tracts, 
+      mostly on brown-black soil which had ber and babul vegetation.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Unlike the other chalcolithic farming cultures, Ahar (Between C. 2,100 and 1,500 B.C.) practically did not use microlithic tools. 
+      State true or false?`,
+      answer: `True`,
+      description: `<ul>
+      <li>Stone axes or blades were almost absent.</li>
+      <li>Almost all of its objects were made of copper.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Black-and-red pottery was widely prevalent during the chalcolithic phase. State true of false?`,
+      answer: `True`,
+      description: `<ul>
+      <li>It was thrown on wheel and occasionally painted with white linear designs.</li>
+      <li>Considering the pre-bronze phase of chalcolithic cultures, chalcolithic people seems to be the first ones to use painted pottery.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Though people of the chalcolithic age domesticated animals and practised agriculture, they generally were not 
+      acquainted with _____?`,
+      answer: `Horse`,
+      description: `<ul>
+      <li>Some animal remains are identified as belonging either to the horse or donkey or wild ass.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `People of the chalcolithic age produced wheat and rice. State true or false?`,
+      answer: `True`,
+      description: `<ul>
+      <li>They also produced bajra, ber, linseed, cotton (mainly in Deccan), ragi, bajra, millets, and several pulses 
+      (such as the lentil, black gram, green gram, and grass pea.)</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Generally, the houses of the chalcolithic people were made of _____ and _____ ?`,
+      answer: `Wattle and daub`,
+      description: `<ul>
+      <li>Burnt bricks were seldom used.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `The largest Jorwe site discoverd so far is _____ in the Godavari valley?`,
+      answer: `Daimabad`,
+      description: `<ul>
+      <li>Seems to have been fortified with a mud wall.</li>
+      <li>Large number of bronze goods have been discovered from here.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `At Inamgaon, a house of later chalcolithic phase with 4 _____ shaped rooms and 1 _____ shaped room, have been discovered?`,
+      answer: `4 rectangular and 1 circular`,
+      description: `<ul>
+      <li>Of the earlier chalcolithic phase, large mud houses with ovens and circular pit houses, have been discovered from here.</li>
+      <li>A granary lies close to it.</li>
+      <li>Despite the Inamgaon being a large chalcolithic settlement with more than 100 houses and numerous burials, 
+      it was fortified and surrounded by a moat.</li>
+      <li>Works of potters, smiths, ivory carvers, lime makers, and terracotta artisans were found at Inamgaon.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Chalcolithic people manufactured beads of semi-precious stones such as carnelian, steatite, and quartz crystal. 
+      State true or false?`,
+      answer: `True`,
+      description: `<ul>
+      <li>Copper bangles were also recovered.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Chalcolithic people didn't knew the art of spinning and weaving. State true or false?`,
+      answer: `False`,
+      description: `<ul>
+      <li>They knew spinning and weaving. Spindle whorls have been discovered in Malwa.</li>
+      <li>Cotton, flax, and silk threads have been found in Maharashtra.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `In which age/phase did regional differences with regard to cereals, pottery, etc., appear?`,
+      answer: `Stone-copper or Chalcolithic phase`,
+      description: `<ul>
+      <li>Western India cultivated barley and wheat, while eastern India produced rice.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Chalcolithic people of Maharashtra buried their dead in urns under the _____ in the north-to-south position?`,
+      answer: `Floor of their house`,
+      description: `<ul>
+      <li>Unlike Harappans, they did not use separate cemeteries.</li>
+      <li>Valuables like pots and some copper objects were deposited in the graves.</li>
+      <li>In south India, dead body was placed in the east-west position.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `_____ suggests that the mother goddess was venerated by the chalcolithic people?`,
+      answer: `Terracotta figures of women`,
+      description: `<ul>
+      <li>Some unbaked nude clay figurines were also used for worship.</li>
+      <li>Stylized bull terracottas suggests that the bull was the symbol of a religious cult.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `There were no social inequalities in the chalcolithic society. State true or false?`,
+      answer: `False`,
+      description: `<ul>
+      <li>The settlement pattern and burial practices of the chalcolithic people suggest beginning of
+      social inequalities.</li>
+      <li>It is believed that the chief and his kinsmen who lived in rectanular houses dominated others who lived in round huts.</li>
+      <li>In Inamgaon, while the chief lived probably in the centre, the craftsmen lived in the western fringes.</li>
+      <li>While the grave goods of affluent consisted of bangles, necklaces of semi-precious stones, and copper objects, 
+      the grave goods of some others consisted of only pots.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Traces of chalcolithic cultures have been discovered almost all over the country, except for the 
+      _____ and the _____?`,
+      answer: `Alluvial plains and thickly forested areas`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Since the neolithic phase imperceptibly faded into the chalcolithic phase in south India, these cultures are called _____?`,
+      answer: `Neolithic-chalcolithic`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Plough and hoe were found at chalcolithic sites. State true or false?`,
+      answer: `False`,
+      description: `<ul>
+      <li>Neither plough nor hoe has been found at chalcolithic sites.</li>
+      <li>Perforated stone discs were tied as weights to the digging sticks.</li>
+      </ul>`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Chalcolithic people did not know the art of writing. State true or false?`,
+      answer: `True`
+    },
+    {
+      qaNumber: this.qaNumber++,
+      question: `Ochre-coloured pottery (OCP) culture period may roughly be placed in the range _____ B.C. to _____ B.C.?`,
+      answer: `2,000 B.C. to 1,500 B.C.`,
+      description: `<ul>
+      <li>OCP people lived in areas not far removed from that of the Harappans.</li>
+      </ul>`
+    },
   ];
-
 }
--- src/app/general-awareness-qa/history-qa/ancient-indian-history-qa/stone-age-qa/stone-age-qa.component.ts
+++ src/app/general-awareness-qa/history-qa/ancient-indian-history-qa/stone-age-qa/stone-age-qa.component.ts
@@ -20,17 +20,21 @@ export class StoneAgeQAComponent implements OnInit {
       qaNumber: this.qaNumber++,
       question: `Which period represents the time during which recognizable modern humans evolved?`,
       answer: `Quaternary period`,
-      description: `<p>Quaternary period is often considered the "Age of Humans". The quaternary period began
-       2.588 million years ago and extends into the present. It's a part of the Cenozoic Era.<p>
-       <p>But now, though not officially accepted, appearance of modern man seems to have occured in Africa 
-       about 3 million years ago.<p>`
+      description: `<ul>
+      <li>Quaternary period is often considered the "Age of Humans". The quaternary period began
+      2.588 million years ago and extends into the present. It's a part of the Cenozoic Era.</li>
+      <li>But now, though not officially accepted, appearance of modern man seems to have occured in Africa 
+      about 3 million years ago.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
       question: `Quaternary Period is divided into two epochs. Which are they?`,
       answer: `Pleistocene Epoch and Holocene Epoch`,
-      description: `<p>Pleistocene Epoch: 2.588 million years ago to 11,700 years ago<p> 
-              <p>Holocene Epoch: 11,700 years ago to present<p>`
+      description: `<ul>
+      <li>Pleistocene Epoch: 2.588 million years ago to 11,700 years ago</li>
+      <li>Holocene Epoch: 11,700 years ago to present</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
@@ -40,12 +44,14 @@ export class StoneAgeQAComponent implements OnInit {
       <li>Mesolithic age</li>
       <li>Neolithic age</li>
       </ol>`,
-      description: `<p>Palaeolithic age: Considered to have begun in India around 500,000 B.C. 
-      Humans were hunters and food gatherers.<p>
-      <p>Mesolithic age: Considered to have begun in India around 9,000 B.C. 
-      Humans were largely hunters and herders.<p>
-      <p>Neolithic age: Considered to have begun in India around 5,000 B.C. 
-      Humans became food producers.<p>`
+      description: `<ul>
+      <li>Palaeolithic age: Considered to have begun in India around 500,000 B.C. 
+      Humans were hunters and food gatherers.</li>
+      <li>Mesolithic age: Considered to have begun in India around 9,000 B.C. 
+      Humans were largely hunters and herders.</li>
+      <li>Neolithic age: Considered to have begun in India around 5,000 B.C. 
+      Humans became food producers.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
@@ -56,38 +62,46 @@ export class StoneAgeQAComponent implements OnInit {
       <li>Middle Palaeolithic</li>
       <li>Upper Palaeolithic</li>
       </ol>`,
-      description: `<p>Early or Lower Palaeolithic: Roughly between 500,000 B.C. and 50,000 B.C.
-      Hand-axes, cleavers, and choppers were used.<p>
-      <p>Middle Palaeolithic: Roughly between 50,000 B.C. and 40,000 B.C. 
-      Blades, points, borers, and scrapers made of flakes were used.<p>
-      <p>Upper Palaeolithic: Roughly between 40,000 B.C. and 10,000 B.C. 
-      Blades, burins, scrapers, and flakes were used.<p>`
+      description: `<ul>
+      <li>Early or Lower Palaeolithic: Roughly between 500,000 B.C. and 50,000 B.C.
+      Hand-axes, cleavers, and choppers were used.</li>
+      <li>Middle Palaeolithic: Roughly between 50,000 B.C. and 40,000 B.C. 
+      Blades, points, borers, and scrapers made of flakes were used.</li>
+      <li>Upper Palaeolithic: Roughly between 40,000 B.C. and 10,000 B.C. 
+      Blades, burins, scrapers, and flakes were used.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
       question: `Since the fossils of early men have not been found in India, _____ obtained from the excavations
       are being considered as the evidence of earliest human presence?`,
       answer: `Stone tools`,
-      description: `<p>The early man in India used stone tools roughly dressed by crude chipping. Since man did not had 
+      description: `<ul>
+      <li>The early man in India used stone tools roughly dressed by crude chipping. Since man did not had 
       knowledge of cultivation and house building during the stone age, chipped stone tools were mainly used for hunting,
-       cutting, and gathering food.<p>
-      <p>As of now, earliest human presence in India, based on the analysis of stone tools, is dated to somewhere between 
-      500,000 B.C and 250,000 B.C.<p>
-      <p>Though the evolution of Stone tools and related technology is similar to that in Africa, it is believed 
-      that modern humans appeared in India much later than in Africa.`
+       cutting, and gathering food.</li>
+      <li>As of now, earliest human presence in India, based on the analysis of stone tools, is dated to somewhere between 
+      500,000 B.C and 250,000 B.C.</li>
+      <li>Though the evolution of Stone tools and related technology is similar to that in Africa, it is believed 
+      that modern humans appeared in India much later than in Africa.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
       question: `Palaeolithic stone tools used by the early man in India have been discovered throughout the country except the _____?`,
       answer: `Alluvial plains of Indus, Ganga, and Yamuna rivers`,
-      description: `Palaeolithic sites are mainly found in river valleys and hill slopes.`
+      description: `<ul>
+      <li>Palaeolithic sites are mainly found in river valleys and hill slopes.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
       question: `_____ are the characteristic tools of the Mesolithic age?`,
       answer: `Microliths`,
-      description: `A microlith is a minute stone tool, usually made of flint, typically used as a part of a composite tool such as 
-      a spear or arrow.`
+      description: `<ul>
+      <li>A microlith is a minute stone tool, usually made of flint, typically used as a part of a composite tool such as 
+      a spear or arrow.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
@@ -110,33 +124,43 @@ export class StoneAgeQAComponent implements OnInit {
       qaNumber: this.qaNumber++,
       question: `With more than 500 painted rock shelters, _____ in Madhya Pradesh is a striking site having prehistoric art?`,
       answer: `Bhimbetka`,
-      description: `The time period of rock paintings spans from the Palaeolithic to recent times. Human beings, animals, and birds are painted.`
+      description: `<ul>
+      <li>The time period of rock paintings spans from the Palaeolithic to recent times. Human beings, animals, and birds are painted.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
       question: `_____ in Baluchistan is the only neolithic settlement in the Indian subcontinent attributed to 7,000 B.C.`,
       answer: `Mehrgarh`,
-      description: `Considering world as a whole, neolithic age is believed to have begun by 9,000 B.C. But, in India, generally, neolithic 
-      settlements are considered to range between 5,000 B.C. and 1,000 B.C.`
+      description: `<ul>
+      <li>Considering world as a whole, neolithic age is believed to have begun by 9,000 B.C. But, in India, generally, neolithic 
+      settlements are considered to range between 5,000 B.C. and 1,000 B.C.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
       question: `People of Neolithic age generally used tools and implements of _____?`,
       answer: `Polished stone`,
-      description: `Stone axes need special mention, as they were very popular.`
+      description: `<ul>
+      <li>Stone axes need special mention, as they were very popular.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
       question: `Which neolithic culture was distinguished by its dwelling pits, and the complete absence of the microliths?`,
       answer: `Kashmiri neolithic culture`,
-      description: `<p>It is distinguished by its range of ceramics, and the variety of stone, and bone tools as well.<p>
-      <p>Burzahom and Gufkral are two such prominent sites.<p>`
+      description: `<ul>
+      <li>It is distinguished by its range of ceramics, and the variety of stone, and bone tools as well.</li>
+      <li>Burzahom and Gufkral are two such prominent sites.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
       question: `Apart from Kashmir, _____ is the only place in India which yielded considerable neolithic bone implements?`,
       answer: `Chirand`,
-      description: `These bone implements were mostly made of antlers (horns of deer).`
+      description: `<ul>
+      <li>These bone implements were mostly made of antlers (horns of deer).</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
@@ -155,8 +179,10 @@ export class StoneAgeQAComponent implements OnInit {
       question: `The neolithic people who lived to the south of Godavari had the knowledge of making/using fire-baked earthern figurines 
       as well as rubbing stone querns. State true or false?`,
       answer: `True`,
-      description: `<p>Fire-baked earthern figurines recovered suggests that they possesed cattle, sheep, and goats.<p>
-      <p>Rubbing stone querns recovered suggests that they were acquainted with the art of producing cereals.<p>`
+      description: `<ul>
+      <li>Fire-baked earthern figurines recovered suggests that they possesed cattle, sheep, and goats.</li>
+      <li>Rubbing stone querns recovered suggests that they were acquainted with the art of producing cereals.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
@@ -180,25 +206,31 @@ export class StoneAgeQAComponent implements OnInit {
       qaNumber: this.qaNumber++,
       question: `Pottery first appears in which phase of the stone age?`,
       answer: `Neolithic phase`,
-      description: `<p>Settled life necesseciated the use of pots for storing foodgrains, cooking, eating, and drinking.<p>
-      <p>Hand-made pottery is found in the early neolithic phase while footwheel turned pottery emerged towards later neolithic phase.<p>
-      <p>Black-burnished ware, grey ware, and mat-impressed ware were used.<p>`
+      description: `<ul>
+      <li>Settled life necesseciated the use of pots for storing foodgrains, cooking, eating, and drinking.</li>
+      <li>Hand-made pottery is found in the early neolithic phase while footwheel turned pottery emerged towards later neolithic phase.</li>
+      <li>Black-burnished ware, grey ware, and mat-impressed ware were used.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
       question: `From the perspective of general progress in technology, the neolithic age in Indian subcontinent can be said to have 
       begun around _____ millennium B.C.?`,
       answer: `Sixth millennium B.C.`,
-      description: `<p>Rice, wheat, and barley started to be cultivated.<p>
-      <p>Villages started appearing.<p>`
+      description: `<ul>
+      <li>Rice, wheat, and barley started to be cultivated.</li>
+      <li>Villages started appearing.</li>
+      </ul>`
     },
     {
       qaNumber: this.qaNumber++,
       question: `The people of stone age could settle down only on the hilly river valleys, slopes of the hills, and in the rock-shelters.
       State True or False?`,
       answer: `True`,
-      description: `<p>With just tools and weapons made of stone, and bones, they could not establish settlements far away from the hilly areas.<p>
-      <p>Also, they could not produce much more than what they needed for their subsistence.<p>`
+      description: `<ul>
+      <li>With just tools and weapons made of stone, and bones, they could not establish settlements far away from the hilly areas.</li>
+      <li>Also, they could not produce much more than what they needed for their subsistence.</li>
+      </ul>`
     },
   ];
 }
