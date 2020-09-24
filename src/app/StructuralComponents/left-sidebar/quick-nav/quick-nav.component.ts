af6bd05f0da5bbb887287af5de5c17046810f7e7
JibinJF <jibinjf@gmail.com>
2020-09-24 15:53:26 +0530

    Left Quick Nav

--- src/app/StructuralComponents/left-sidebar/quick-nav/quick-nav.component.ts
+++ src/app/StructuralComponents/left-sidebar/quick-nav/quick-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QAWbListModel } from '../../qawb-list-model';
 
 @Component({
   selector: 'app-quick-nav',
@@ -12,4 +13,27 @@ export class QuickNavComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  wbNumber: number = 1;  
+  qaWbList: QAWbListModel[] = [
+    {
+      wbNumber: this.wbNumber++,
+      wbName: `Economics QA`,
+      wbRoute: '/EconomicsQA'
+    },
+    {
+      wbNumber: this.wbNumber++,
+      wbName: `Geography QA`,
+      wbRoute: '/GeographyQA'
+    },
+    {
+      wbNumber: this.wbNumber++,
+      wbName: `History QA`,
+      wbRoute: '/HistoryQA'
+    },
+    {
+      wbNumber: this.wbNumber++,
+      wbName: `Political Science QA`,
+      wbRoute: '/PoliticalScienceQA'
+    },
+  ];
 }
