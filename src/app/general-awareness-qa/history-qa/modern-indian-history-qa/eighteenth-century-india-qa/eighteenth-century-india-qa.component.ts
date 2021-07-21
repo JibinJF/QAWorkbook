0ef4507d360a92dadd7a27733f7171be75c141ae
JibinJF <57614642+JibinJF@users.noreply.github.com>
2021-07-21 21:53:15 +0530

    Update eighteenth-century-india-qa.component.ts

--- src/app/general-awareness-qa/history-qa/modern-indian-history-qa/eighteenth-century-india-qa/eighteenth-century-india-qa.component.ts
+++ src/app/general-awareness-qa/history-qa/modern-indian-history-qa/eighteenth-century-india-qa/eighteenth-century-india-qa.component.ts
@@ -21,35 +21,33 @@ export class EighteenthCenturyIndiaQAComponent implements OnInit {
   eighteenthCenturyIndiaQAs: QAModel[] = [
     {
       qaNumber: this.qaNumber++,
-      question: `?`,
-      answer: `.`,
+      question: `Mughal emperor Aurangzeb died in the year _____ ?`,
+      answer: `1707.`,
       description: `<ul>
-      <li>.</li>
+      <li>Aurangzeb died on 1707 March 3.</li>
       </ul>`
     },
     {
       qaNumber: this.qaNumber++,
-      question: `?`,
-      answer: `.`,
+      question: `On Aurangzeb's death, his 3 sons fought among themselves for the throne 
+      and _____ emerged victorious?`,
+      answer: `Bahadur Shah.`,
       description: `<ul>
-        <li></li>
+        <li>Bahadur Shah died in Lahore on 1712 February 27.</li>
+        <li>He was the only Mughal Emperor who used the title of Sayyid. His full name 
+        was 'Abul-nasr Sayyid Qutb-ud-din Muhammad Shah Alam Bahadur Shah Badshah'.</li>
       </ul>`
     },
     {
       qaNumber: this.qaNumber++,
-      question: `?`,
-      answer: `.`,
-      description: `<ul>
-       <li></li>
-      </ul>`
+      question: `_____, historically, is a tax paid by non-Muslim populations to their Muslim rulers?`,
+      answer: `Jizyah (also spelled jizya).`,
     },
     {
       qaNumber: this.qaNumber++,
-      question: `?`,
-      answer: `.`,
-      description: `<ul>
-        <li></li>
-      </ul>`
+      question: `In 1679 _____ reintroduced the jizyah, a poll tax for non-Muslims that had been 
+      abolished by Akbar the Great a century earlier?`,
+      answer: `Aurangzeb.`,
     },
     {
       qaNumber: this.qaNumber++,
